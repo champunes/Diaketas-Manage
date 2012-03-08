3b87da37afc0646479a0f2fb7ac59399390a9195
Jose Angel Gonzalez Molina <champuness@gmail.com>
2012-03-08 19:57:51 +0100

    Netbeans

---
+++ NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/Main.java
@@ -0,0 +1,17 @@
+package Controlador;
+
+/**
+ *
+ * @author champunes
+ */
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		
+		System.out.println("Hello world!");
+	}
+}
