149e89e97169dc151da9e3338ea0e45862153e50
Jobero <Jobero@Jobero-PC>
2012-03-21 12:40:36 +0100

    Creacion de clases 1

---
+++ NetBeansProjects/Diaketas-Manage_IT1/src/Clases/Beneficiario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Jobero
+ */
+public class Beneficiario extends Persona {
+    
+    
+    private int CP;
+    private String domicilio;
+    private String estadoCivil;
+    private String localidad;
+    private String nacionalidad;
+    private int nivelDeEstudio;
+    private String observaciones;
+    private String ocupacion;
+    private String profesion;
+    private String situacionEconomica;
+    private String vivienda;
+    private float viviendaAlquiler;
+    private String viviendaObservaciones;
+    
+    private Vector familiares;
+    
+}
---
+++ NetBeansProjects/Diaketas-Manage_IT1/src/Clases/Modifiacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jobero
+ */
+public class Modifiacion {
+    private Date fecha;
+    
+}
