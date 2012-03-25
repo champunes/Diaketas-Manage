5f9ea6e316bef10c5c86f3d9a4a724b37a59f94e
Mario Orozco Borrego <mariottf@hotmail.com>
2012-03-25 18:15:07 +0200

    Finalización del diseño de la interfaz, comentadas todas las clases de interfaz generadas.

--- NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/ControladorContabilidad.java
+++ NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/ControladorContabilidad.java
@@ -29,8 +29,6 @@ package Controlador;
 import Vista.VentanaPrincipal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 
 public class ControladorContabilidad implements ActionListener{
