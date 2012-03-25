1669a0a8fd3a97337ff44b1a88cdc3f8bbb9fbb4
Mario Orozco Borrego <mariottf@hotmail.com>
2012-03-25 18:14:12 +0200

    Finalización del diseño de la interfaz, comentadas todas las clases de interfaz generadas.

--- NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/ControladorAyuda.java
+++ NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/ControladorAyuda.java
@@ -1,3 +1,28 @@
+/**
+ ** NOMBRE CLASE: 
+ **	  ControladorAyuda.java
+ **
+ ** DESCRIPCION:
+ **       Controlador del panel de ayudas
+ **       
+ **
+ ** DESARROLLADO POR:
+ *          Mario Orozco Borrego (MOB)
+ **        
+ **
+ ** SUPERVISADO POR:
+ **          
+ **
+ ** HISTORIA:
+ ** 	000 - Mar 22, 2012 - MOB - Creacion
+ *      001 - Mar 22, 2012 - MOB - Reestructuración de la clase, ahora implementa ActionListener
+ *      002 - Mar 23, 2012 - MOB - Modificación del método Overriden actionPerformed de la interfaz para que gestione las diferentes acciones de la UI
+ *      
+ **
+ ** NOTAS:
+ **   
+ **
+ */
 
 package Controlador;
 
@@ -7,48 +32,52 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-/**
- * @see ControladorVolunt es una clase singleton
- * @author Mario Orozco Borrego
- */
 
 public class ControladorAyuda implements ActionListener{
 
-	//Atributo para singleton
-	private static ControladorAyuda instancia = null;
-	
-	public static ControladorAyuda getInstance(){
-		
-		if(instancia == null)
-			instancia = new ControladorAyuda();
-		return instancia;
-		
-	}
-	
-	private VentanaPrincipal vista;
-      
+    /** PATRON DE DISEÑO SINGLETON */
+    
+    private static ControladorAyuda instancia = null;
 
-	public void setVentanaPrincipal(VentanaPrincipal pvista){
-         vista = pvista;
-        }	
-	
-	private ControladorAyuda(){
-		
-	}
-	
+    public static ControladorAyuda getInstance(){
 
+            if(instancia == null)
+                    instancia = new ControladorAyuda();
+            return instancia;
 
+    }
+
+    private VentanaPrincipal vista;
+
+    /** 
+    * Establece como ventana padre la pasada como parámetro
+    * @param pvista ventana padre
+    */
+    public void setVentanaPrincipal(VentanaPrincipal pvista){
+        vista = pvista;
+    }	
+
+    /** 
+     * Constructor de la clase 
+     */
+    private ControladorAyuda(){
+
+    }
+
+    /**
+     * Manejador de eventos de la interfaz
+     * @param ae evento
+     */ 
     @Override
     public void actionPerformed(ActionEvent ae) {
         String sAccion = ae.getActionCommand();
         
-      
         /** PANEL DE AYUDAS **/
         if (sAccion.equals("navToMainFromAyudas")){
             vista.mostrarVistaPrincipal();
         }
         else if (sAccion.equals("navToVoluntariosFromAyudas")){
-            vista.mostrarVistaVolunt();
+            vista.mostrarVistaVoluntarios();
         }
         else if (sAccion.equals("buscarBeneficiario")){
             
@@ -63,9 +92,6 @@ public class ControladorAyuda implements ActionListener{
             
         }
       
-        else{
-
-        }
     }
 	
 }
--- NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/ControladorBeneficiario.java
+++ NetBeansProjects/Diaketas-Manage_IT1/src/Controlador/ControladorBeneficiario.java
@@ -1,3 +1,29 @@
+/**
+ ** NOMBRE CLASE: 
+ **	  ControladorBeneficiario.java
+ **
+ ** DESCRIPCION:
+ **        Controlador del panel de beneficiarios, asímismo de algunas operaciones que ocurren dentro de su jerarquía de paneles
+ **       
+ **
+ ** DESARROLLADO POR:
+ *          Mario Orozco Borrego (MOB)
+ **         José Ángel González Molina (JGM)
+ **        
+ **
+ ** SUPERVISADO POR:
+ **          
+ **
+ ** HISTORIA:
+ ** 	000 - Mar 20, 2012 - JGM - Creacion
+ *      001 - Mar 22, 2012 - MOB - Reestructuración de la clase, ahora implementa ActionListener
+ *      002 - Mar 23, 2012 - MOB - Modificación del método Overriden actionPerformed de la interfaz para que gestione las diferentes acciones de la UI
+ *      
+ **
+ ** NOTAS:
+ **   
+ **
+ */
 
 package Controlador;
 
@@ -5,13 +31,11 @@ import Vista.VentanaPrincipal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * @see ControladorBenef es una clase singleton
- * @author Mario Orozco Borrego
- */
+
 public class ControladorBeneficiario implements ActionListener{
 
-	//Atributo para singleton
+	/** PATRON DE DISEÑO SINGLETON */
+    
 	private static ControladorBeneficiario instancia = null;
 	
 	public static ControladorBeneficiario getInstance(){
@@ -22,22 +46,27 @@ public class ControladorBeneficiario implements ActionListener{
 		
 	}
 	
-	private VentanaPrincipal vista;
+    private VentanaPrincipal vista;
 	
+    /**
+     * Constructor de la clase
+     */
+    private ControladorBeneficiario(){
+    
+    }
 	
-	private ControladorBeneficiario(){
-		
-	}
-	
+    /** 
+     * Establece como ventana padre la pasada como parámetro
+     * @param pvista ventana padre
+     */
      public void setVentanaPrincipal(VentanaPrincipal pvista){
          vista = pvista;
      }
    
-	
-	public void mostrarVista(){
-		vista.mostrarVistaBenef();
-	}
-
+     /**
+      * Manejador de eventos de la interfaz
+      * @param ae evento
+      */
     @Override
     public void actionPerformed(ActionEvent ae) {
         
@@ -46,10 +75,10 @@ public class ControladorBeneficiario implements ActionListener{
         System.out.println("ActionCommand: " + sAccion);
         
         if (sAccion.equals("buscarBeneficiario")){
-            vista.mostrarVistaBBuscar();
+            vista.mostrarVistaBuscarBeneficiario();
         }
         else if (sAccion.equals("nuevoBeneficiario")){
-            vista.mostrarVistaNBenef();
+            vista.mostrarVistaDatosBeneficiario();
         }
         else if (sAccion.equals("navToMainFromBeneficiarios")){
             vista.mostrarVistaPrincipal();
@@ -60,7 +89,7 @@ public class ControladorBeneficiario implements ActionListener{
             vista.mostrarVistaPrincipal();
         }
         else if (sAccion.equals("navToBeneficiariosFromBuscarBeneficiario")){
-            vista.mostrarVistaBenef();
+            vista.mostrarVistaBeneficiarios();
         }
         else if (sAccion.equals("buscarBeneficiarioDNI")){
             
@@ -77,12 +106,9 @@ public class ControladorBeneficiario implements ActionListener{
             vista.mostrarVistaPrincipal();
         }
         else if (sAccion.equals("navToBeneficiariosFromBeneficiarioDatos")){
-            vista.mostrarVistaBenef();
+            vista.mostrarVistaBeneficiarios();
         }
-        
-        else {
-            
-        }
+ 
     }
 	
 }
